35dec2435c4e0a366152ef70fc60008adca5310c
hvu76 <hvu@agnesscott.edu>
2021-06-19 04:14:35 +0700

    Debug some coding errors

--- site/simon.js
+++ site/simon.js
@@ -5,12 +5,37 @@ let redBox= document.getElementById("red");
 let yellowBox= document.getElementById("yellow");
 let blueBox= document.getElementById("blue");
 
+
+
 let colorList = ['red','blue','blue','green'];
 let playList = [];
 
 bigBox = document.querySelector('.simon-container');
 
+/*bigBox.onclick = function(){
+    numClicked++;
+    document.querySelector('#clicktimes').innerHTML = numClicked;
+}*/
+
 document.querySelector('#colors').innerHTML = colorList[0]
+let numClicked = 0;
+
+function compare(listOriginal, listPlayer){
+    for(i=0; i<listPlayer.length; i++){
+        if(listPlayer[i] === listOriginal[i]){
+            document.querySelector('.result').innerHTML = 'Congrats!';
+            document.querySelector('#colors').innerHTML = listOriginal[i+1];
+            if((i+1)>=listOriginal.length){
+                document.querySelector('.result').innerHTML = 'Congrats! You win!';
+                document.querySelector('.order').innerHTML = "No more clicks!";
+            }
+        }
+        else{
+            document.querySelector('.result').innerHTML = 'Game Over!'; 
+        }
+    }
+
+}
 
 bigBox.addEventListener('click', function(event){
     tile = event.target.id;
@@ -18,18 +43,6 @@ bigBox.addEventListener('click', function(event){
     playList.push(tile);
     console.log(playList);
 
-    for(i=0; i<=playList.length; i++){
-        if(playList[i] === colorList[i]){
-            document.querySelector('.result').innerHTML = 'Congrats!';
-            document.querySelector('#colors').innerHTML = colorList[i+1];
-            if((i+1)>colorList.length){
-                document.querySelector('.result').innerHTML = 'Congrats! You win!';
-                document.querySelector('#colors').innerHTML = "";
-            }
-        }
-        else{
-            document.querySelector('.result').innerHTML = 'Game Over!';
-        }
-    }
-
+    compare(colorList, playList)
+    
 })
