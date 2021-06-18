a49c0bf05ab5bac5857a2e60964b5232872242d4
hvu76 <hvu@agnesscott.edu>
2021-06-19 03:00:20 +0700

    add code to index.html, main.css, buttons.js, and simon.js
    Make the buttons light on and off when clicking them.
    Figure out the v4 section in readme.md aka create a simple version of the Simon game

--- site/buttons.js
+++ site/buttons.js
@@ -1,19 +1,47 @@
 let greenButton = document.querySelector('.simon-button.green');
-greenButton.addEventListener('click', function() {
-  alert('You clicked the green button');
-});
+greenButton.addEventListener('click', function(event) {
+  flashGreen();
+})
 
 let redButton = document.querySelector('.simon-button.red');
 redButton.addEventListener('click', function() {
-  alert('You clicked the red button');
+  flashRed();
 });
 
 let yellowButton = document.querySelector('.simon-button.yellow');
 yellowButton.addEventListener('click', function() {
-  alert('You clicked the yellow button');
+  flashYellow();
 });
 
 let blueButton = document.querySelector('.simon-button.blue');
 blueButton.addEventListener('click', function() {
-  alert('You clicked the blue button');
+  flashBlue();
 });
+
+function flashGreen() {
+  greenButton.classList.add('highlight1');
+  setTimeout(function(){
+      greenButton.classList.remove('highlight1');
+  }, 200);
+}
+
+function flashRed() {
+  redButton.classList.add('highlight2');
+  setTimeout(function(){
+      redButton.classList.remove('highlight2');
+  }, 200)
+}
+
+function flashYellow() {
+  yellowButton.classList.add('highlight3');
+  setTimeout(function(){
+      yellowButton.classList.remove('highlight3');
+  }, 200)
+}
+
+function flashBlue() {
+  blueButton.classList.add('highlight4');
+  setTimeout(function(){
+      blueButton.classList.remove('highlight4');
+  }, 200)
+}
--- site/simon.js
+++ site/simon.js
@@ -1,2 +1,35 @@
 // This file contains the game logic.
 // All the event-listening should happen in buttons.js
+let greenBox= document.getElementById("green");
+let redBox= document.getElementById("red");
+let yellowBox= document.getElementById("yellow");
+let blueBox= document.getElementById("blue");
+
+let colorList = ['red','blue','blue','green'];
+let playList = [];
+
+bigBox = document.querySelector('.simon-container');
+
+document.querySelector('#colors').innerHTML = colorList[0]
+
+bigBox.addEventListener('click', function(event){
+    tile = event.target.id;
+    console.log(tile);
+    playList.push(tile);
+    console.log(playList);
+
+    for(i=0; i<=playList.length; i++){
+        if(playList[i] === colorList[i]){
+            document.querySelector('.result').innerHTML = 'Congrats!';
+            document.querySelector('#colors').innerHTML = colorList[i+1];
+            if((i+1)>colorList.length){
+                document.querySelector('.result').innerHTML = 'Congrats! You win!';
+                document.querySelector('#colors').innerHTML = "";
+            }
+        }
+        else{
+            document.querySelector('.result').innerHTML = 'Game Over!';
+        }
+    }
+
+})
